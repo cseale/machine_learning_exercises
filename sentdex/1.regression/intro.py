c3808dfbcc2866b3dc950ba5e255be15158a05fc
Colm Seale <colm.seale@gmail.com>
2017-01-12 01:00:09 +0000

    added prediction capability and graphing

--- sentdex/1.regression/intro.py
+++ sentdex/1.regression/intro.py
@@ -4,9 +4,14 @@
 import pandas as pd
 import quandl
 import math
+import datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -26,29 +31,44 @@ print('Number of Days in Advance: ' + str(forecast_out))
 # shift the data by a certain number of days in order to build predictions
 # so the next days stock price (adj close) becomes the prediction for that day
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
+
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
 
 # use linear regression
 # sklearn.linear_model.LinearRegression
-clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
-print('Accuracy of LinearRegression Classifier: ' + str(accuracy))
+forecast_set = clf.predict(X_lately);
 
+print (forecast_set, accuracy, forecast_out)
 
-# use support vector machine
-# uses different kernals
-clf2 = svm.SVR()
-clf2.fit(X_train, y_train)
-accuracy2 = clf2.score(X_test, y_test)
+df['Forecast'] = np.nan
 
-print('Accuracy of SVM Classifier: ' + str(accuracy2))
+# create dates from graph
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
