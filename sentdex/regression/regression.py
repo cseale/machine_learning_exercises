b29006bf0561cc51d589ca76c724845228d99c89
Colm Seale <colm.seale@gmail.com>
2017-01-19 22:26:51 +0000

    [LinearRegression] added y intercept, plot and predict

---
+++ sentdex/regression/regression.py
@@ -0,0 +1,37 @@
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+xs = np.array([1 ,2 ,3 ,4 ,5 ,6], dtype=np.float64)
+ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+
+# plt.scatter(xs, ys)
+# plt.show()
+
+def best_fit_slope_and_intercept(xs, ys):
+    m = ( ((mean(xs) * mean(ys)) - mean(xs*ys) ) /
+            ((mean(xs)**2) - mean(xs**2)))
+    b = mean(ys) - m*(mean(xs))
+    return m, b
+
+m, b = best_fit_slope_and_intercept(xs, ys)
+
+regression_line = [(m*x) + b for x in xs]
+
+# same as doing the following:
+# for x in xs:
+#     regression_line.append( (m*x) + b )
+
+# regression  plot
+plt.scatter(xs, ys)
+plt.plot(xs, regression_line)
+
+# prediction
+predict_x = 8;
+predict_y = (m * predict_x) + b
+
+plt.scatter(predict_x, predict_y, color='g')
+plt.show()
